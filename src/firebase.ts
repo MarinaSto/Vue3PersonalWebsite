c226b2c919e43ac67f115dc736580a8660c28765
MarinaStojmenova <marina.stojmenova@hotmail.it>
2022-04-08 23:34:01 +0200

    Uniform naming with backend for episode

--- src/firebase.ts
+++ src/firebase.ts
@@ -29,50 +29,53 @@ const firebaseApp = initializeApp(firebaseConfig);
 const firestore = getFirestore(firebaseApp);
 
 class Episode {
-  id: string;
-  title: string;
-  episodeNumber: number;
-  videoUrl: string;
-  description: string;
-  gallery: Array<string>;
-  dbRef: DocumentReference<DocumentData>;
-  constructor(episode: QueryDocumentSnapshot<DocumentData>) {
-    if (
-      episode.data().episode == null ||
-      typeof episode.data().episode != "number"
-    ) {
-      throw new Error("got a malformed episode field." + episode);
+
+  constructor(
+    public dbRef: DocumentReference<DocumentData>,
+    public id: string,
+    public title: string,
+    public episodeNumber: number,
+    public videoUrl: string,
+    public description: string,
+    public distance: number,
+    public downhill: number,
+    public duration: number,
+    public maxAltitude: number,
+    public summary: string,
+    public uphill: number,
+    // public gallery: Array<string>,
+  ) {
+    console.log("Constructor")
+  }
+  static validation() {
+    return {
+      title: "string",
+      episodeNumber: "number",
+      videoUrl: "string",
+      description: "string",
+      distance: "number",
+      downhill: "number",
+      duration: "number",
+      maxAltitude: "number",
+      summary: "string",
+      uphill: "number",
     }
-    if (
-      episode.data().title == null ||
-      typeof episode.data().title != "string"
-    ) {
-      throw new Error("got a malformed title field." + episode);
+  }
+  static createFromDocument(episode_document: QueryDocumentSnapshot<DocumentData>): Episode {
+    const data = episode_document.data();
+    const validation = this.validation()
+    let property: keyof typeof validation
+    const episodeParam = []
+    for (property in validation) {
+      if (
+        data[property] == null ||
+        typeof data[property] != validation[property]
+      ) {
+        throw new Error(`got a malformed ${property} field while parsing ${episode_document.id}`);
+      }
+      episodeParam.push(data[property])
     }
-    if (
-      episode.data().video_url == null ||
-      typeof episode.data().video_url != "string"
-    ) {
-      throw new Error("got a malformed videoUrl field." + episode);
-    }
-    this.id = episode.id;
-    this.dbRef = episode.ref;
-    this.episodeNumber = episode.data().episode;
-    this.title = episode.data().title;
-    this.videoUrl = episode.data().video_url;
-    this.title = "Ciclabile dell' Alta val di Non";
-    this.description = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamu
-                euismod commodo tellus, a lobortis dolor elementum quis.
-                Vestibulum eget ipsum non sem scelerisque consequat. Orci varius
-                natoque penatibus et magnis dis parturient montes, nascetur
-                ridiculus mus. Fusce sit amet nisl a nibh sagittis dapibus in ac
-                metus. Pellentesque et arcu a nibh aliquam malesuada. Quisque
-                malesuada dictum nunc non semper. Curabitur ut auctor metus.
-                Vivamus tristique diam in auctor lobortis. Duis elit dui,
-                scelerisque at mi eget, placerat volutpat tortor. Curabitur
-                ligula leo, laoreet non porta id, sollicitudin in nulla. In hac
-                habitasse platea dictumst. Aenean in vulputate metus.`;
-    this.gallery = [];
+    return new Episode(episode_document.ref, episode_document.id, ...episodeParam);
   }
 }
 class Season {
@@ -99,13 +102,13 @@ class Season {
     if (season.season == null || typeof season.season != "number") {
       throw new Error("got a malformed season field." + season);
     }
-    if (season.playlist == null || typeof season.playlist != "string") {
+    if (season.playlist_url == null || typeof season.playlist_url != "string") {
       throw new Error("got a malformed playlist field." + season);
     }
     this.nEpisodes = season.n_episodes;
     this.seasonNumber = season.season;
     this.year = season.year;
-    this.playlist = season.playlist;
+    this.playlist = season.playlist_url;
   }
   activateRealtimeUpdate() {
     this.unsubscribe = onSnapshot(this.dbRef, (doc) => {
@@ -121,13 +124,14 @@ class Season {
       this.unsubscribe = undefined;
     }
   }
+
   async getEpisodes(): Promise<Episode[]> {
     const episodes: Episode[] = [];
     const episodesCol = collection(this.dbRef, "episodes");
     const episodesSnapshot = await getDocs(episodesCol);
     episodesSnapshot.docs.forEach((episode) => {
       try {
-        episodes.push(new Episode(episode));
+        episodes.push(Episode.createFromDocument(episode));
       } catch (err: any) {
         const { message } = err;
         console.error("Skipping episode because: ", message);
@@ -137,19 +141,19 @@ class Season {
   }
   async getEpisodeByNumber(
     episodeNumber: number
-  ): Promise<Episode | undefined> {
+  ): Promise<Episode> {
     const episodeCol = collection(this.dbRef, "episodes");
-    const q = query(episodeCol, where("episode", "==", episodeNumber));
+    const q = query(episodeCol, where("episodeNumber", "==", episodeNumber));
     const querySnapshot = await getDocs(q);
     if (querySnapshot.empty) {
-      return undefined;
+      throw new Error(`Cannot find episode with number ${episodeNumber} in season ${this.id}`);
     }
     if (querySnapshot.size > 1) {
       console.warn(
         `Multiple episodes with number ${episodeNumber} in season ${this.seasonNumber} received. Selecting the first one and discarding the others.`
       );
     }
-    return new Episode(querySnapshot.docs[0]);
+    return Episode.createFromDocument(querySnapshot.docs[0]);
   }
   static async getByNumber(seasonNumber: number): Promise<Season | undefined> {
     const seasonsCol = collection(firestore, "seasons");
